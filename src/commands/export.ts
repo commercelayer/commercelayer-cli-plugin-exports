4dee2d847f6f009c7829416a4faab0559eb4dfde
Pierluigi Viti <pierluigi@commercelayer.io>
2021-05-05 19:01:45 +0200

    Create export command skeleton

--- src/commands/export.ts
+++ src/commands/export.ts
@@ -1,25 +1,21 @@
-import {Command, flags} from '@oclif/command'
+import { Command, flags } from '@oclif/command'
 
 export default class Export extends Command {
+
   static description = 'describe the command here'
 
   static flags = {
-    help: flags.help({char: 'h'}),
-    // flag with a value (-n, --name=VALUE)
-    name: flags.string({char: 'n', description: 'name to print'}),
-    // flag with no value (-f, --force)
-    force: flags.boolean({char: 'f'}),
+    // help: flags.help({ char: 'h' }),
+    
   }
 
-  static args = [{name: 'file'}]
+  static args = []
 
   async run() {
-    const {args, flags} = this.parse(Export)
 
-    const name = flags.name ?? 'world'
-    this.log(`hello ${name} from /Users/pierlu/Documents/GitHub/commercelayer-cli-plugin-exporter/src/commands/export.ts`)
-    if (args.file && flags.force) {
-      this.log(`you input --force and --file: ${args.file}`)
-    }
+    const { args, flags } = this.parse(Export)
+
+
   }
+
 }
